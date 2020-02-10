b2a82631d623abf2a05eed2d46dadae963abd3db
Kris-S <kristopher@wix.com>
2020-02-10 14:18:56 -0500

    Update languageAlert.js
    Remove notes

--- AgentReplyLangAlert/languageAlert.js
+++ AgentReplyLangAlert/languageAlert.js
@@ -1,22 +1,3 @@
-//LIMITATIONS
-
-//Checks agent reply AFTER it's been sent.
-//Doesn't actually do anything besides a window alert.
-//Doesn't catch variations of words such as @$$ or f*ck.
-//REGEX or using a "bad" word library might be better than array of "bad" words.
-
-//IMPROVEMENT IDEAS
-
-//Run code every 1000ms using setInterval
-  //Hide Reply button if bad word present
-    // .reply-buttons
-  //Show Reply buttons if bad word NOT present
-   // .reply-buttons
- //Show warning message if bad word present.
- //Hide warning message if bad word NOT present.
-
-
-
 answersBackofficeSdk.addListener(answersBackofficeSdk.eventTypes.ticketReplied, function (replyContent) {
 
   //stores reply object content
